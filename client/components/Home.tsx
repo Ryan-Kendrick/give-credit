cbe40ed1f8574efa4fe380c40871200fd751d1a6
Ryan Kendrick <ryan.kendrick93@gmail.com>
2023-03-18 20:48:44 +1300

    add router and form input

---
+++ client/components/Home.tsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1>Tax Calculator Project</h1>
+}
+
+export default Home
---
+++ client/components/Input.tsx
@@ -0,0 +1,43 @@
+import { ChangeEvent, useState, FormEvent } from 'react'
+
+interface FormData {
+  income: number
+}
+
+function Input() {
+  const [formData, setFormData] = useState({
+    income: 0,
+  })
+
+  function changeHandler(e: ChangeEvent<HTMLInputElement>) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const submitHandler = (e: FormEvent) => {
+    e.preventDefault()
+    console.log('submitted')
+  }
+
+  return (
+    <div className="form-cont">
+      <form onSubmit={submitHandler}>
+        <label htmlFor="Income">Income:</label>
+        <input
+          id="input-income"
+          name="income"
+          type="number"
+          value={formData.income}
+          onChange={changeHandler}
+        />
+        <button type="submit" className="formButton">
+          Go
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default Input
