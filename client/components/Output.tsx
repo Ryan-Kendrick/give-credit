87bf551001a873d0949670c785776166635126a8
Ryan Kendrick <ryan.kendrick93@gmail.com>
2023-07-22 10:42:57 +1200

    add timeline template

--- client/components/Output.tsx
+++ client/components/Output.tsx
@@ -151,67 +151,94 @@ function Output({ incomeData }: Props) {
   const displayPlaceholder = () => {
     return (
       <>
-        <div className="basis-1/2 font-subheading mt-2 bg-white leading-5">
-          <h2 className="font-bold p-2">
+        <div className="basis-1/2 font-subheading mt-2 ml-3 mr-4 bg-white leading-5">
+          <h2 className="font-bold p-1">
             Getting a refund is easier than you might think
           </h2>
-          <p>
+          <p className="mb-2">
             All you have to do is click a button to open a donation tax credit
-            account, upload receipts and you&apos;re done.
+            account, upload receipts and you&apos;re done:
           </p>
-          {/*
-          
-<ol class="items-center sm:flex">
-    <li class="relative mb-6 sm:mb-0">
-        <div class="flex items-center">
-            <div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                </svg>
-            </div>
-            <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-        </div>
-        <div class="mt-3 sm:pr-8">
-            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Flowbite Library v1.0.0</h3>
-            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released on December 2, 2021</time>
-            <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements.</p>
-        </div>
-    </li>
-    <li class="relative mb-6 sm:mb-0">
-        <div class="flex items-center">
-            <div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                </svg>
-            </div>
-            <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-        </div>
-        <div class="mt-3 sm:pr-8">
-            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Flowbite Library v1.2.0</h3>
-            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released on December 23, 2021</time>
-            <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements.</p>
-        </div>
-    </li>
-    <li class="relative mb-6 sm:mb-0">
-        <div class="flex items-center">
-            <div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                </svg>
-            </div>
-            <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-        </div>
-        <div class="mt-3 sm:pr-8">
-            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Flowbite Library v1.3.0</h3>
-            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released on January 5, 2022</time>
-            <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements.</p>
-        </div>
-    </li>
-</ol>
 
-            */}
+          <ol className="items-center sm:flex">
+            <li className="relative mb-6 sm:mb-0">
+              <div className="flex items-center my-2">
+                <div className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white sm:ring-8 shrink-0">
+                  <svg
+                    className="w-2.5 h-2.5 text-blue-800"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                  </svg>
+                </div>
+                <div className="hidden sm:flex w-full bg-gray-200 h-0.5 "></div>
+              </div>
+              <div className="mt-3 sm:pr-8">
+                <h3 className="text-lg font-semibold text-gray-900">
+                  Flowbite Library v1.0.0
+                </h3>
+                <p className="text-base font-normal text-gray-500">
+                  Get started with dozens of web components and interactive
+                  elements.
+                </p>
+              </div>
+            </li>
+            <li className="relative mb-6 sm:mb-0">
+              <div className="flex items-center">
+                <div className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white sm:ring-8 shrink-0">
+                  <svg
+                    className="w-2.5 h-2.5 text-blue-800"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                  </svg>
+                </div>
+                <div className="hidden sm:flex w-full bg-gray-200 h-0.5"></div>
+              </div>
+              <div className="mt-3 sm:pr-8">
+                <h3 className="text-lg font-semibold text-gray-900">
+                  Flowbite Library v1.2.0
+                </h3>
+                <p className="text-base font-normal text-gray-500">
+                  Get started with dozens of web components and interactive
+                  elements.
+                </p>
+              </div>
+            </li>
+            <li className="relative mb-6 sm:mb-0">
+              <div className="flex items-center">
+                <div className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white sm:ring-8 shrink-0">
+                  <svg
+                    className="w-2.5 h-2.5 text-blue-800"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                  </svg>
+                </div>
+                <div className="hidden sm:flex w-full bg-gray-200 h-0.5"></div>
+              </div>
+              <div className="mt-3 sm:pr-8">
+                <h3 className="text-lg font-semibold text-gray-900">
+                  Flowbite Library v1.3.0
+                </h3>
+                <p className="text-base font-normal text-gray-500">
+                  Get started with dozens of web components and interactive
+                  elements.
+                </p>
+              </div>
+            </li>
+          </ol>
         </div>
-        <div className="basis-1/2 mt-2 font-subheading leading-5">
+        <div className="basis-1/2 mt-2 ml-3 mr-4 font-subheading leading-5">
           <h2 className="font-bold p-2">More info</h2>
           <p className="">
             You can be refunded 33.33 cents for every dollar donated to{' '}
