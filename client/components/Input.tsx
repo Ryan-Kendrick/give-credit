ab5ec28e88214ded8dadc2bef05cb3923af82a4f
Ryan Kendrick <ryan.kendrick93@gmail.com>
2023-08-06 11:11:13 +1200

    add ternary for kiwisaver dropdown toggle

--- client/components/Input.tsx
+++ client/components/Input.tsx
@@ -152,36 +152,39 @@ function Input(props: Props) {
           aria-haspopup="true"
           onClick={() => toggleDropdown('KiwiSaver')}
         >
-          <svg
-            className="w-5 h-5 text-gray-400"
-            fill="currentColor"
-            height="64px"
-            width="64px"
-            version="1.1"
-            id="Layer_1"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 330 330"
-            xmlSpace="preserve"
-          >
-            <path
-              id="XMLID_222_"
-              d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001
+          {formData.display.KiwiSaver ? (
+            <svg
+              className="w-5 h-5 text-gray-400"
+              width="64px"
+              height="64px"
+              fill="currentColor"
+              viewBox="0 0 1024 1024"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M903.232 256l56.768 50.432L512 768 64 306.432 120.768 256 512 659.072z" />
+            </svg>
+          ) : (
+            <svg
+              className="w-5 h-5 text-gray-400"
+              fill="currentColor"
+              height="64px"
+              width="64px"
+              version="1.1"
+              id="Layer_1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 330 330"
+              xmlSpace="preserve"
+            >
+              <path
+                id="XMLID_222_"
+                d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001
 	c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213
 	C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606
 	C255,161.018,253.42,157.202,250.606,154.389z"
-            />
-          </svg>
-          {/* <svg
-            className="w-5 h-5 text-gray-400"
-            width="64px"
-            height="64px"
-            fill="currentColor"
-            viewBox="0 0 1024 1024"
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M903.232 256l56.768 50.432L512 768 64 306.432 120.768 256 512 659.072z" />
-          </svg> */}
+              />
+            </svg>
+          )}
         </button>
       </div>
     )
